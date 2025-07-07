88b1ff504dd69c923d8ba38b79074970bb5e7da8
BigBookZa <hlopmaria57@gmail.com>
2025-07-07 21:32:02 +0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-eexport default defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
