7e3e4645e2189ce61536740ffc813256ae2a63a4
BigBookZa <hlopmaria57@gmail.com>
2025-07-07 21:20:38 +0500

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+eexport default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  server: {
+    proxy: {
+      '/uploads': {
+        target: 'http://localhost:3001', // если бекенд на этой же машине
+        // если бекенд на другом сервере, поставь IP и порт нужный!
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 });
